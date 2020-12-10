11603649c7329d7d305b4bcd90af37910b6df7d7
Frederik Schubert <frederik.schubert@inside-m2m.de>
2020-12-10 15:56:17 +0100

    remove solution

--- policy_gradient.py
+++ policy_gradient.py
@@ -21,18 +21,9 @@ class Policy(nn.Module):
 
     def __init__(self, observation_space, action_space, hidden_size=128):
         super().__init__()
-        self.fc1 = nn.Linear(np.prod(observation_space.shape), hidden_size)
-        self.fc2 = nn.Linear(hidden_size, action_space.n)
 
     def forward(self, x):
-        if not torch.is_tensor(x):
-            x = torch.tensor(x)
-        x = x.float()
-        x = self.fc1(x)
-        x = F.relu(x)
-        logits = self.fc2(x)
-        probs = F.softmax(logits, dim=-1)
-        return probs
+        pass
 
 
 policy = Policy(env.observation_space, env.action_space)
@@ -41,35 +32,15 @@ optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 
 def compute_returns(rewards, discount_factor=DISCOUNT_FACTOR):
     """Compute discounted returns"""
-    returns = []
-    return_to_go = 0
-    for r in rewards[::-1]:
-        return_to_go = r + discount_factor * return_to_go
-        returns.insert(0, return_to_go)
-    return returns
+
 
 
 def policy_improvement(log_probs, rewards):
     """Compute REINFORCE policy gradient and perform gradient ascent step"""
-    returns = torch.tensor(compute_returns(rewards))
-    advantages = (returns - returns.mean()) / (
-        torch.std(returns) + np.finfo(np.float32).eps
-    )
-    log_probs = torch.stack(log_probs)
-    optimizer.zero_grad()
-    loss = -torch.sum(log_probs * advantages)
-    loss.backward()
-    optimizer.step()
-    return loss.item()
 
 
 def act(state):
     """ Use policy to sample an action and return probability for gradient update"""
-    probs = policy(state)
-    distribution = Categorical(probs=probs)
-    action = distribution.sample()
-    log_prob = distribution.log_prob(action)
-    return action, log_prob
 
 
 def policy_gradient(num_episodes):
